427da43b0f9683b4130fded4ff267de3d0979a9a
AdnaNation <azizulislamadnan@gmail.com>
2024-06-05 20:29:31 +0600

    firebase installed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,47 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import SectionTitle from "../components/SectionTitle";
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const name = data.name;
+    const email = data.email;
+    const role = data.role;
+    const image = data.image[0];
+    const password = data.password;
+    const formData = new FormData();
+
+    formData.append("image", image);
+    try {
+      const { data } = await axios.post(image_hosting_api, formData);
+      console.log(data.data.display_url);
+    } catch (err) {
+      console.log(err);
+    }
+    // try {
+    //   const res = await axios.post(image_hosting_api, formData);
+    //   const data = res.data;
+    //   if (data.success) {
+    //     const image = data.data.url;
+
+    //     setImageUrl(image);
+    //   } else {
+    //     console.error("Error uploading image:", data.error.message);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
   return (
     <section className="min-h-screen">
       <div className="w-full md:max-w-3xl mx-auto mt-10 p-4 rounded-md shadow sm:p-8 dark:bg-gray-50 dark:text-gray-800">
@@ -8,61 +49,124 @@ const SignUp = () => {
 
         <div className="my-6 space-y-4"></div>
 
-        <form noValidate="" action="" className="space-y-8">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
           <div className="space-y-4">
-            <div className="space-y-2">
-              <label htmlFor="email" className="block text-sm">
-                Email address
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">User Name</span>
+              </label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                name="name"
+                placeholder="User Name"
+                className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
+              />
+              {errors.name && (
+                <span className="text-red-600">Name is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                {...register("email", { required: true })}
                 name="email"
-                id="email"
-                placeholder="leroy@jenkins.com"
+                placeholder="Your Email"
                 className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
               />
+              {errors.email && (
+                <span className="text-red-600">Email is required</span>
+              )}
             </div>
-            <div className="space-y-2">
-              <div className="flex justify-between">
-                <label htmlFor="password" className="text-sm">
-                  Password
+            <div className="flex flex-col md:flex-row gap-6">
+              <div className="form-control w-full ">
+                <label className="label">
+                  <span className="label-text">Role</span>
                 </label>
+                <select
+                  defaultValue="user"
+                  {...register("role", { required: true })}
+                  className="select select-bordered w-full"
+                >
+                  <option value="user">User</option>
+                  <option value="seller">Seller</option>
+                </select>
               </div>
+
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Upload Your Photo</span>
+                </label>
+                <input
+                  {...register("image", { required: true })}
+                  type="file"
+                  className="file-input w-full "
+                />
+                {errors.image && (
+                  <span className="text-red-600">Photo is required</span>
+                )}
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
               <input
                 type="password"
-                name="password"
-                id="password"
-                placeholder="*****"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                })}
+                placeholder="Password"
                 className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
               />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600">Password must be 6 characters</p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-600">
+                  Password must be less than 20 characters
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must have one Uppercase one lower case, one number
+                  and one special character.
+                </p>
+              )}
             </div>
           </div>
-          <div className="flex items-center w-full my-4">
-            <hr className="w-full dark:text-gray-600" />
-            <p className="px-3 dark:text-gray-600">OR</p>
-            <hr className="w-full dark:text-gray-600" />
+          <div className="form-control mt-6">
+            <input className="btn btn-primary" type="submit" value="Sign Up" />
           </div>
-          <button
-            aria-label="Login with Google"
-            type="button"
-            className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              className="w-5 h-5 fill-current"
-            >
-              <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-            </svg>
-            <p>Login with Google</p>
-          </button>
-          <button
-            type="button"
-            className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
-          >
-            Sign in
-          </button>
         </form>
+        <div className="flex items-center w-full my-4">
+          <hr className="w-full dark:text-gray-600" />
+          <p className="px-3 dark:text-gray-600">OR</p>
+          <hr className="w-full dark:text-gray-600" />
+        </div>
+        <button
+          aria-label="Login with Google"
+          type="button"
+          className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            className="w-5 h-5 fill-current"
+          >
+            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+          </svg>
+          <p>Login with Google</p>
+        </button>
       </div>
     </section>
   );
